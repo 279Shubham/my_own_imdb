cf6f4e369620477af2f732c416eb390ad3c2b41f
B0206315 <shubham1.sharma@airtel.com>
2018-08-01 21:35:51 +0530

    ios project created

---
+++ my_own_imdb/my_own_imdb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my_own_imdb
+//
+//  Created by B0206315 on 01/08/18.
+//  Copyright © 2018 Kashmir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
